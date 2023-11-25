2ddc7577597fa2bb428cd753dc3cb3d47c5145b9
Brandon <brandontang0325@gmail.com>
2023-11-25 13:47:28 +0000

    Update textdisplay to use new parent state

--- src/components/TextDisplay.tsx
+++ src/components/TextDisplay.tsx
@@ -3,8 +3,9 @@ import Word from "./Word";
 
 const TextDisplay: React.FC<{
   allWords: string[];
-  currentIndex: number;
-}> = ({ allWords, currentIndex }) => {
+  typingWordIndex: number;
+  typingCharIndex: number;
+}> = ({ allWords, typingWordIndex, typingCharIndex }) => {
   const currentWordRef = useRef<HTMLElement | null>(null);
   const scrollerRef = useRef<HTMLDivElement>(null);
   const checkScrollRef = useRef(true);
@@ -32,34 +33,6 @@ const TextDisplay: React.FC<{
     }
   });
 
-  function findWordAndIndex(words: string[], totalCharIndex: number) {
-    let currentCharIndex = 0;
-
-    for (let i = 0; i < words.length; i++) {
-      const word = words[i];
-      const wordLength = word.length;
-
-      // Check if the totalCharIndex falls within the current word
-      if (totalCharIndex < currentCharIndex + wordLength) {
-        const charIndexRelativeToWord =
-          totalCharIndex - currentCharIndex;
-        return { typingWordIndex: i, charIndexRelativeToWord };
-      }
-
-      // Move to the next word
-      currentCharIndex += wordLength + 1; // Assuming a space between words
-    }
-
-    // If the totalCharIndex is beyond the last word, return the last word's index
-    return {
-      typingWordIndex: words.length - 1,
-      charIndexRelativeToWord: totalCharIndex - currentCharIndex,
-    };
-  }
-
-  const { typingWordIndex, charIndexRelativeToWord } =
-    findWordAndIndex(allWords, currentIndex);
-
   return (
     <>
       <div className="text-display">
@@ -71,7 +44,7 @@ const TextDisplay: React.FC<{
               isTyped={i < typingWordIndex}
               isTyping={i == typingWordIndex}
               currentCharIndex={
-                i == typingWordIndex ? charIndexRelativeToWord : -1
+                i == typingWordIndex ? typingCharIndex : -1
               }
               ref={i == typingWordIndex ? currentWordRef : null}
             />
