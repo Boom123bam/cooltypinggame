87cbb95ac86ea6d6fac7d2e53b42795e5498b5e5
Brandon <brandontang0325@gmail.com>
2023-11-28 09:41:12 +0000

    Add mode selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import ModeSelector from "./components/ModeSelector";
 import TypingGame from "./components/TypingGame";
 
 function App() {
   return (
     <>
+      <ModeSelector />
       <TypingGame />
     </>
   );
---
+++ src/components/ModeSelector.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+interface ModeOptions {
+  time: number[];
+  words: number[];
+  infinite: null;
+}
+
+interface SelectedState {
+  mode: keyof ModeOptions;
+  value: number | null;
+}
+
+const options: ModeOptions = {
+  time: [15, 30, 60],
+  words: [10, 25, 50],
+  infinite: null,
+};
+
+function ModeSelector() {
+  const [selected, setSelected] = useState<SelectedState>({
+    mode: "time",
+    value: 15,
+  });
+
+  function handleMajorOptionClick(option: keyof ModeOptions) {
+    if (selected.mode != option) {
+      const optionValues = options[option];
+      setSelected({
+        mode: option,
+        value:
+          optionValues && optionValues.length > 0
+            ? optionValues[0]
+            : null,
+      });
+    }
+  }
+
+  function handleMinorOptionClick(option: number) {
+    setSelected({ ...selected, value: option });
+  }
+
+  return (
+    <div className="mode-selector">
+      <div className="major-options">
+        {Object.keys(options).map((option) => (
+          <button
+            className="option"
+            aria-selected={
+              selected.mode === option ? "true" : "false"
+            }
+            onClick={() =>
+              handleMajorOptionClick(option as keyof ModeOptions)
+            }
+          >
+            {option}
+          </button>
+        ))}
+        <span className="selection-indicator"></span>
+      </div>
+      <div className="minor-options">
+        {options[selected.mode]?.map((option) => (
+          <button
+            className="option"
+            aria-selected={
+              selected.value === option ? "true" : "false"
+            }
+            onClick={() => handleMinorOptionClick(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ModeSelector;
