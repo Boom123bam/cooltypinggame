051d12c88453bdb68cd1a591afc291a0fecd7001
Brandon <brandontang0325@gmail.com>
2023-12-05 11:46:47 +0000

    Add animated gradient backgrount to card

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -2,7 +2,21 @@ import { StatsProps } from "../types/types";
 
 function Stats({ show }: StatsProps) {
   return (
-    <div className={`stats ${show ? "show" : "hide"}`}>stats</div>
+    <div className={`stats ${show ? "show" : "hide"}`}>
+      <div className="glow-card animated-gradient">
+        <div className="wpm">
+          <div className="big">60</div>
+          <div className="small">wpm</div>
+        </div>
+
+        <hr />
+
+        <div className="restart">
+          <button>restart</button>
+          <div className="enter">enter</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
