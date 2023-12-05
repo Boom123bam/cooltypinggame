7d8ef5e61f654aa01a192dc76eab77593f905517
Brandon <brandontang0325@gmail.com>
2023-12-05 15:27:59 +0000

    add card transition

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -3,19 +3,21 @@ import { StatsProps } from "../types/types";
 function Stats({ show }: StatsProps) {
   return (
     <div className={`stats ${show ? "show" : "hide"}`}>
-      <div className="glow-card animated-gradient glow-shadow">
-        <div className="inner">
-          <div className="wpm">
-            <div className="number">60</div>
-            <small>wpm</small>
-          </div>
-          <hr />
+      <div className="card-wrapper">
+        <div className="glow-card animated-gradient glow-shadow">
+          <div className="inner">
+            <div className="wpm">
+              <div className="number">60</div>
+              <small>wpm</small>
+            </div>
+            <hr />
 
-          <div className="restart">
-            <button>
-              <img src="icons/restart.svg" alt="restart" />
-            </button>
-            <small className="enter">enter</small>
+            <div className="restart">
+              <button>
+                <img src="icons/restart.svg" alt="restart" />
+              </button>
+              <small className="enter">enter</small>
+            </div>
           </div>
         </div>
       </div>
