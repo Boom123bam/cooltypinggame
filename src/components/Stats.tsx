ab330d54ed5387bafc036e2b455e44401f455d1d
Brandon <brandontang0325@gmail.com>
2023-12-09 09:36:28 +0000

    Add sick card hover effect

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -2,12 +2,63 @@ import { useEffect, useRef } from "react";
 import { useLastKey } from "../hooks/zustand/useLastKey";
 import { useGameState } from "../hooks/zustand/useGameState";
 import { useStats } from "../hooks/zustand/useStats";
+import gsap from "gsap";
 
 function Stats() {
   const { lastKeyPressed, lastKeyUpdateFlag } = useLastKey();
   const { setIsFinished } = useGameState();
   const { wpm, accuracy } = useStats();
   const cardWrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!cardWrapperRef.current) return;
+    const bounds = cardWrapperRef.current.getBoundingClientRect();
+
+    function handleMouseMove(e: MouseEvent) {
+      if (!cardWrapperRef.current) return;
+
+      const leftX = e.clientX - bounds.x;
+      const topY = e.clientY - bounds.y;
+      const mouseX = leftX - bounds.width / 2;
+      const mouseY = topY - bounds.height / 2;
+
+      gsap.to(cardWrapperRef.current, {
+        ["--mouse-x"]: mouseX,
+        ["--mouse-y"]: mouseY,
+        duration: 0.2,
+      });
+    }
+
+    function handleMouseLeave() {
+      gsap.to(cardWrapperRef.current, {
+        ["--mouse-x"]: 0,
+        ["--mouse-y"]: 0,
+        duration: 0.2,
+      });
+    }
+
+    cardWrapperRef.current.addEventListener(
+      "mousemove",
+      handleMouseMove
+    );
+
+    cardWrapperRef.current.addEventListener(
+      "mouseleave",
+      handleMouseLeave
+    );
+
+    return () => {
+      cardWrapperRef.current?.removeEventListener(
+        "mousemove",
+        handleMouseMove
+      );
+      cardWrapperRef.current?.removeEventListener(
+        "mouseleave",
+        handleMouseLeave
+      );
+    };
+  }, []);
+
   useEffect(() => {
     if (lastKeyPressed == "Enter") reset();
   }, [lastKeyUpdateFlag]);
