75e2c4fd2f72abf25b69acc43f8ff30bf49a8725
Brandon <brandontang0325@gmail.com>
2023-12-12 10:02:17 +0000

    Disable tab index when typing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,17 @@ function Footer() {
   return (
     <>
       <footer className={isTyping ? "hide" : ""}>
-        <a href="https://cooltypinggame.com">cooltypinggame.com</a>
+        <a
+          href="https://cooltypinggame.com"
+          tabIndex={isTyping ? -1 : 1}
+        >
+          cooltypinggame.com
+        </a>
         <a
           href="https://github.com/Boom123bam/cooltypinggame"
           target="blank"
           className="github"
+          tabIndex={isTyping ? -1 : 1}
         >
           <img src="github.svg" alt="" />
         </a>
